3f88928f59cd1c28ca3c6037d8ca68cd230b9d4b
gabvaj987 <vajdag314@gmail.com>
2015-04-19 13:48:40 +0200

    fixed ship definition resource access

--- src/main/java/app/Battle.java
+++ src/main/java/app/Battle.java
@@ -9,33 +9,33 @@ import ship.ShipFleet;
 
 public class Battle {
 
-	private static final int SIZE = 20;
+    private static final int SIZE = 20;
 
-	public static void main(String[] args) {
-		// Set up field
-		Field field = new LimitedSizeField(SIZE, SIZE);
-		ShipPositioner shipPositioner = new ShipPositioner(field);
+    public static void main(final String[] args) {
+        // Set up field
+        Field field = new LimitedSizeField(SIZE, SIZE);
+        ShipPositioner shipPositioner = new ShipPositioner(field);
 
-		ShipLoader shipLoader = new ShipLoader("ships.txt");
-		List<ShipFleet> loadFleets = shipLoader.loadFleets();
+        ShipLoader shipLoader = new ShipLoader("/ships.txt");
+        List<ShipFleet> loadFleets = shipLoader.loadFleets();
 
-		for (ShipFleet shipFleet : loadFleets) {
-			shipPositioner.addShips(shipFleet);
-		}
+        for (ShipFleet shipFleet : loadFleets) {
+            shipPositioner.addShips(shipFleet);
+        }
 
-		System.out.println(field);
+        System.out.println(field);
 
-		// hit ships
-		boolean hit;
-		for (int i = 0; i < SIZE / 3; i++) {
-			for (int j = 0; j < SIZE; j++) {
-				Position pos = new Position(i, j);
-				hit = field.fire(pos);
-				if (hit == true) {
-					System.out.println("hit at: " + pos);
-				}
-			}
-		}
-		System.out.println(field);
-	}
+        // hit ships
+        boolean hit;
+        for (int i = 0; i < SIZE / 3; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                Position pos = new Position(i, j);
+                hit = field.fire(pos);
+                if (hit == true) {
+                    System.out.println("hit at: " + pos);
+                }
+            }
+        }
+        System.out.println(field);
+    }
 }
--- src/main/java/app/ShipLoader.java
+++ src/main/java/app/ShipLoader.java
@@ -1,8 +1,11 @@
 package app;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,68 +14,69 @@ import ship.ShipFleet;
 import ship.ShipShape;
 
 public class ShipLoader {
-	private final String fileName;
+    private final String fileName;
 
-	public ShipLoader(String fileName) {
-		this.fileName = fileName;
-	}
+    public ShipLoader(final String fileName) {
+        this.fileName = fileName;
+    }
 
-	public List<ShipFleet> loadFleets() {
-		List<ShipFleet> fleets = null;
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			fleets = readFleets(br);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return fleets;
-	}
+    public List<ShipFleet> loadFleets() {
+        List<ShipFleet> fleets = null;
+        URL url = getClass().getResource(fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(url.toURI())))) {
+            fleets = readFleets(br);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return fleets;
+    }
 
-	private List<ShipFleet> readFleets(BufferedReader br) throws IOException {
-		List<ShipFleet> shipFleets = new ArrayList<>();
-		List<Position> positions = new ArrayList<>();
-		String s = br.readLine();
-		int j = 0;
-		while (s != null) {
-			if (!s.matches("^\\d+")) {
-				List<Position> shapeRow = readShapeRow(s, j);
-				positions.addAll(shapeRow);
-				j++;
-			} else {
-				ShipFleet newFleet = createFleet(positions, s);
-				if (newFleet != null) {
-					shipFleets.add(newFleet);
-				}
-				j = 0;
-			}
-			s = br.readLine();
-		}
-		return shipFleets;
-	}
+    private List<ShipFleet> readFleets(final BufferedReader br) throws IOException {
+        List<ShipFleet> shipFleets = new ArrayList<>();
+        List<Position> positions = new ArrayList<>();
+        String s = br.readLine();
+        int j = 0;
+        while (s != null) {
+            if (!s.matches("^\\d+")) {
+                List<Position> shapeRow = readShapeRow(s, j);
+                positions.addAll(shapeRow);
+                j++;
+            } else {
+                ShipFleet newFleet = createFleet(positions, s);
+                if (newFleet != null) {
+                    shipFleets.add(newFleet);
+                }
+                j = 0;
+            }
+            s = br.readLine();
+        }
+        return shipFleets;
+    }
 
-	private List<Position> readShapeRow(String s, int j) {
-		List<Position> positions = new ArrayList<>();
-		int i = 0;
-		String[] split = s.split(" ");
-		for (String string : split) {
-			if (string.equals("x")) {
-				positions.add(new Position(i, j));
-			}
-			i++;
-		}
-		return positions;
-	}
+    private List<Position> readShapeRow(final String s, final int j) {
+        List<Position> positions = new ArrayList<>();
+        int i = 0;
+        String[] split = s.split(" ");
+        for (String string : split) {
+            if (string.equals("x")) {
+                positions.add(new Position(i, j));
+            }
+            i++;
+        }
+        return positions;
+    }
 
-	private ShipFleet createFleet(List<Position> positions, String s) {
-		ShipFleet fleet = null;
-		if (positions.size() > 0) {
-			Position first = positions.get(0);
-			List<Position> normalPositions = new ArrayList<>();
-			for (Position position : positions) {
-				normalPositions.add(position.subtract(first));
-			}
-			fleet = new ShipFleet(new ShipShape(normalPositions), Integer.parseInt(s));
-			positions.clear();
-		}
-		return fleet;
-	}
+    private ShipFleet createFleet(final List<Position> positions, final String s) {
+        ShipFleet fleet = null;
+        if (positions.size() > 0) {
+            Position first = positions.get(0);
+            List<Position> normalPositions = new ArrayList<>();
+            for (Position position : positions) {
+                normalPositions.add(position.subtract(first));
+            }
+            fleet = new ShipFleet(new ShipShape(normalPositions), Integer.parseInt(s));
+            positions.clear();
+        }
+        return fleet;
+    }
 }
