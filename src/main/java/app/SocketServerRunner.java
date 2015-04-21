6519eaab5a28989fa325a38d30d0eb25d3f07f57
gabvaj987 <vajdag314@gmail.com>
2015-04-21 22:35:41 +0200

    Renamed runner

--- src/main/java/app/SocketServerRunner.java
+++ src/main/java/app/SocketServerRunner.java
@@ -3,7 +3,7 @@ package app;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class SocketServer {
+public class SocketServerRunner {
 
     public static void main(final String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(1234)) {
